41d00281b8c0e4fb4af7f2ef75f42a5e6f8fdf44
Trever Anderson <treveranderson01@gmail.com>
2019-12-05 21:31:34 -0600

    can write detections out. network is not good

--- model.py
+++ model.py
@@ -13,6 +13,8 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from datetime import datetime
+from keras.models import load_model
+import keras.backend as kb
 
 SMALL_OBJ = 32 ** 2
 IMAGE_IDS_FILE = 'image_ids.txt'
@@ -31,15 +33,6 @@ def see_dets(resized, ita):
     draw_detection(image=new_img, sx=nx, sy=ny, ex=nx + nw + nh, ey=ny + nw + nh)
     cv2.imwrite('new.jpg', new_img)
 
-def draw_detection(image, sx, sy, ex, ey, lt=2):
-    width = ex - sx
-    height = ey - sy
-    color = (0, 0, 255)
-    cv2.line(image, (sx, sy), (sx + width, sy), color, lt)
-    cv2.line(image, (sx, sy), (sx, sy + height), color, lt)
-    cv2.line(image, (sx + width, sy), (sx + width, sy + height), color, lt)
-    cv2.line(image, (sx, sy + height), (sx + width, sy + height), color, lt)
-
 def rtx_fix():
     tf.logging.set_verbosity(tf.logging.INFO)
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.80)
@@ -55,11 +48,15 @@ def hardware_setup(use_gpu):
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Run detection on images')
-    parser.add_argument('--train-images-path', required=True, help='Path to training images')
+    parser.add_argument('--train-images-path', required=False, help='Path to training images')
     parser.add_argument('--test-images-path', required=True, help='Path to testing images')
     parser.add_argument('--annotations', required=True, help='Path to annotations file')
     parser.add_argument('--epochs', required=False, default=20, type=int)
     parser.add_argument('--use-gpu', required=False, default=False, action='store_true')
+    parser.add_argument('--train', required=False, default=False, action='store_true')
+    parser.add_argument('--predict', required=False, default=False, action='store_true')
+    parser.add_argument('--pretrained-model', required=False, type=str)
+
     return parser.parse_args()
 
 def read_file(path):
@@ -73,11 +70,11 @@ def read_images(image_path, img_id_file, coco_images):
     for i in ids:
         fn = coco_images[i]['file_name']
         result[i] = cv2.imread(path.join(image_path, fn))
-    return result
+    return list(result.items())
 
 def image_to_bboxes(images, coco_obj, target_area):
     result, bad_ids = {}, []
-    for img_id, img_data in images.items():
+    for img_id, img_data in images:
         bboxes = list(map(lambda a: a['bbox'],
                          filter(lambda an: an['area'] < target_area,
                             map(lambda a: coco_obj.anns[a],
@@ -87,7 +84,7 @@ def image_to_bboxes(images, coco_obj, target_area):
         else:
             bad_ids.append(img_id)
         
-    return result, bad_ids
+    return list(result.items()), bad_ids
 
 def load_image(image_path, image_shape):
     image = img_to_array(load_img(image_path, target_size=image_shape))
@@ -106,24 +103,29 @@ def scale_bbox(bbox, orig_shape, to_shape):
 def rescale_bboxes(image_to_bboxes, img_shape):
     resized = {}
     p = 2
-    for img_id, img_bboxes in image_to_bboxes.items():
+    for img_id, img_bboxes in image_to_bboxes:
         data, bboxes = img_bboxes
         rscl_bboxes = list(map(lambda a: scale_bbox(bbox=a, orig_shape=data.shape, 
                                              to_shape=img_shape), bboxes))        
         resized[img_id] = (cv2.resize(data, img_shape), rscl_bboxes)
-    return resized
+    return list(resized.items())
 
 def example_tensors(image_bboxes):
-    xs, ys = zip(*[(data[0], data[1]) for img_id, data in image_bboxes.items()])
+    xs, ys = zip(*[(data[0], data[1]) for img_id, data in image_bboxes])
     return np.array(xs), np.array(ys).reshape((-1, 20))
 
 def create_model(in_shape):
     w, h = in_shape
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(w, h, 3)))
+    model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(w, h, 3)))
     model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+
+    model.add(Conv2D(64, (5, 5), activation='relu', input_shape=(w, h, 3)))
     model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+
     model.add(Flatten())
     model.add(Dense(20, activation='softmax'))
     return model
@@ -134,18 +136,22 @@ def normalize_bbox(im_w, im_h, bbox):
 
 def normalize_bboxes(image_to_bboxes, im_w, im_h):
     normalized = {}
-    for img_id, img_bboxes in image_to_bboxes.items():
+    for img_id, img_bboxes in image_to_bboxes:
         img_data, bboxes = img_bboxes
         norm_bboxes = list(map(lambda b: normalize_bbox(im_w=im_w, im_h=im_h, bbox=b),
                                bboxes))
         normalized[img_id] = (img_data, norm_bboxes)
-    return normalized
+    return list(normalized.items())
 
 def tensors_from_images(images, coco_obj):
     itb, bis = image_to_bboxes(images=images, coco_obj=coco_obj, target_area=SMALL_OBJ)
     rescaled = rescale_bboxes(image_to_bboxes=itb, img_shape=(224, 224))
     normalized = normalize_bboxes(image_to_bboxes=rescaled, im_w=224, im_h=224)
     return example_tensors(image_bboxes=normalized)
+
+def l2_loss(yTrue, yPred):
+    return kb.cast(kb.pow(1, 1), 'int32')
+    #return kb.sum(kb.pow(yTrue - yPred, 2)) / kb.count_params(yTrue)
 
 def train_model(train, test, img_shape, batch_size, epochs):
     tr_xs, tr_ys = train
@@ -166,21 +172,21 @@ def train_model(train, test, img_shape, batch_size, epochs):
 
     model = create_model(in_shape=img_shape)
 
-    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss=l2_loss, metrics=['accuracy'])
     model.fit_generator(train_itr, steps_per_epoch=len(train_itr), epochs=epochs)
     _, acc = model.evaluate_generator(test_itr, steps=len(test_itr), verbose=0)
 
     return model, acc
 
 def save_model(model, fp, epochs):
-    sp = path.join(fp, str(epochs) + '_' + datetime.now().strftime("%d-%m-%y_%H:%M:%S") + '.h5')
+    sp = path.join(fp, str(epochs) + '_' + datetime.now().strftime("%d-%m-%y_%Hh-%Mm-%Ss") + '.h5')
     print("Saving model to {}".format(sp))
     model.save_weights(sp)
 
 def train_runtime(annotations, train_images_path, test_images_path, img_shape,
-                  epochs):
+                  epochs, img_id_file):
     val = COCO(annotations)
-    train, test = list(map(lambda p: read_images(image_path=p, img_id_file=IMAGE_IDS_FILE, 
+    train, test = list(map(lambda p: read_images(image_path=p, img_id_file=img_id_file, 
                                    coco_images=val.imgs), (train_images_path, test_images_path)))
     tr_tensors, tst_tensors = map(lambda ds: tensors_from_images(images=ds, coco_obj=val),
                                   (train, test))
@@ -194,10 +200,91 @@ def train_runtime(annotations, train_images_path, test_images_path, img_shape,
 
     return model
 
+def original_and_tensors(annotations, images_path, img_id_file, img_shape):
+    val = COCO(annotations)
+    images = read_images(image_path=images_path, img_id_file=img_id_file, 
+                                   coco_images=val.imgs)
+    itb, _ = image_to_bboxes(images=images, coco_obj=val, target_area=SMALL_OBJ)
+    xs, ys = tensors_from_images(images=images, coco_obj=val)
+
+    return itb, (xs, ys)
+
+def model_from_disk(model_path, img_shape):
+    model = create_model(img_shape)
+    model.load_weights(model_path)
+
+    return model
+
+def draw_detection(image, sx, sy, w, h, lt=2):
+    ex = sx + w + h
+    ey = sy + w + h
+    width = ex - sx
+    height = ey - sy
+    color = (0, 0, 255)
+    cv2.line(image, (sx, sy), (sx + width, sy), color, lt)
+    cv2.line(image, (sx, sy), (sx, sy + height), color, lt)
+    cv2.line(image, (sx + width, sy), (sx + width, sy + height), color, lt)
+    cv2.line(image, (sx, sy + height), (sx + width, sy + height), color, lt)
+
+def images_to_dets(model, images, tensors): 
+    xs, ys = tensors
+    preds = model.predict(xs)
+
+    preds = preds.reshape((-1, 5, 4))
+
+    num_dets, w, h, _ = xs.shape
+    preds[:, :, 0:4:2] *= w
+    preds[:, :, 1:4:2] *= h
+
+    for i in range(len(preds)):
+        print(preds[i, :, :])
+        input()
+
+    for og_img, i in zip(images, range(len(images))):
+        _, img_bboxes = og_img
+        oi, bboxes = img_bboxes
+        det_img = np.copy(oi)
+        for bbox in bboxes:
+            x, y, w, h = map(lambda i: round(i), bbox)
+            draw_detection(image=det_img, sx=x, sy=y, w=w, h=h)
+        
+        pred_img = xs[i]
+        pred_bboxes = preds[i].tolist()
+        pred_det_img = np.copy(pred_img)
+        print(pred_det_img.shape)
+        for pred_bbox in pred_bboxes:
+            px, py, pw, ph = map(lambda i: round(i), pred_bbox)
+            print((px, py, pw, ph))
+            draw_detection(image=pred_det_img, sx=px, sy=py, w=pw, h=ph)
+
+        cv2.imwrite("og_image_{}.jpg".format(i), det_img)
+        cv2.imwrite("pred_image_{}.jpg".format(i), pred_det_img)
+        input()
+
+    #print("prediction output: {}".format(out.shape))
+    # for i in range(10):
+    #     cv2.imwrite("image_{}.jpg".format(i), images[i][1])
+    #     cv2.imwrite("tensor_equivalent_{}.jpg".format(i), xs[i])
+    #     input()
+
+
+def prediction_runtime(annotations, images_path, img_id_file, img_shape, model_path):
+    images, tensors = original_and_tensors(annotations=annotations, images_path=images_path, 
+                                             img_id_file=img_id_file, img_shape=img_shape)
+    model = model_from_disk(model_path=model_path, img_shape=img_shape)
+    images_to_dets(model=model, images=images, tensors=tensors)
+
+    
 if __name__ == '__main__':
     args = parse_args()
     hardware_setup(use_gpu=args.use_gpu)
-    train_runtime(annotations=args.annotations, train_images_path=args.train_images_path,
-                  test_images_path=args.test_images_path, img_shape=(224, 224), epochs=args.epochs)
-
+    if args.train:
+        train_runtime(annotations=args.annotations, train_images_path=args.train_images_path,
+                      test_images_path=args.test_images_path, img_shape=(224, 224), epochs=args.epochs, 
+                      img_id_file=IMAGE_IDS_FILE)
+    
+    if args.predict:
+        prediction_runtime(annotations=args.annotations, images_path=args.test_images_path, 
+                           img_id_file=IMAGE_IDS_FILE, img_shape=(224, 224), 
+                           model_path=args.pretrained_model)
     
